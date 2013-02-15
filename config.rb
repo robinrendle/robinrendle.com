b12da175fa9b620bc92a40c505454dd777a51c89
Robin Rendle <robinjrendle@gmail.com>
2013-02-15 18:51:27 +0000

    added config.rb to the git ignore

--- config.rb
+++ config.rb
@@ -2,19 +2,21 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "public/css"
+sass_dir = "public/sass"
+images_dir = "public/images"
+javascripts_dir = "public/js"
+fonts_dir = "fonts"
+relative_assets = "true"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
+color_output = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
