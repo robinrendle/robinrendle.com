917aac9940fb2ac1df7d6999dbe75cd89c2926f5
Robin Rendle <robinjrendle@gmail.com>
2013-04-06 22:20:49 +0100

    fixed the header image loop

--- site/templates/about.php
+++ site/templates/about.php
@@ -4,13 +4,13 @@
     <!-- Big image -->
     <?php $hero = $page->images()->find ( html($page->hero()) ) ?>
 
-    <?php if ($page->images()->find ( html($page->hero()) ) ) { ?>
+    <?php if ($page->hero() ) : ?>
         <figure class="hero">
             <img src="<?php echo $hero->url() ?>" alt="<?php echo $hero->title() ?>" />
         </figure>
-    <?php } ?>
+    <?php endif ?>
 
-    <header class="hero">
+    <header class="<?php echo $page->header() ?>">
         <h1><?php echo html($page->title()) ?></h1>
     </header>
 
