555a72e6c5eca3a83b999cbf87c26162221b557b
Robin Rendle <robinjrendle@gmail.com>
2013-04-11 21:42:44 +0100

    added a grid overlay

--- assets/js/onload-ck.js
+++ assets/js/onload-ck.js
@@ -1,3 +1,12 @@
 // DOM Ready - Start
 // ---------------------------------------------------------------------------------------------------
 $(function(){$("h3.contents").click(function(e){e.preventDefault();$("html").toggleClass("contents")})});
+
+
+$(document).ready(function(){
+    $(this).keypress(function(e){
+        if (e.which == 103) {
+            $("header").first().before("<div class='grid'><div class='column'><code>1</code></div><div class='column'><code>1</code></div><div class='column'><code>1</code></div><div class='column'><code>1</code></div><div class='column'><code>1</code></div><div class='column'><code>1</code></div><div class='column'><code>1</code></div><div class='column'><code>1</code></div></div>");
+        };
+    });
+});
