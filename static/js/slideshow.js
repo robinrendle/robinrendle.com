9603ecbec25bb3e4364f5160b5c93e57a510abce
Robin Rendle <robin@erskinedesign.com>
2014-04-29 16:01:54 +0100

    removed a lot of dumb js and updated defaults to be a partial instead

--- static/js/slideshow.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('.infinite-scroll').jscroll({
-        contentSelector: ".slide-content",
-        loadingHtml: "<div class='loading'>Loading</div>"
-    });
-});
