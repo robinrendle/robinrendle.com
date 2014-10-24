2bd8a2d84c7ef2cd240761aa338a074a5c6c2bd1
Robin Rendle <robin@erskinedesign.com>
2014-10-24 12:28:31 +0100

    replaced Grunt with Gulp

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var compass = require('gulp-compass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
+
+gulp.task('scripts', function() {
+    gulp.src([
+            'static/js/vendor/modernizr.custom.js',
+            'static/js/app.js',
+            'static/js/vendor/domReady.js',
+            'static/js/lib/*.js'])
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('compass', function(){
+    return gulp.src('static/sass/*.scss')
+        .pipe(compass({
+            config_file: './config.rb',
+            css: './build/css',
+            sass: './static/sass'
+        }))
+        .pipe(gulp.dest('build/css'))
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('build/css'))
+        .pipe(uglifycss())
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('images', function () {
+    return gulp.src('static/images/*/**')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}]
+            // use: [pngquant()]
+        }))
+        .pipe(gulp.dest('build/images'));
+});
+
+
+gulp.task('watch', function(){
+    gulp.watch('static/js/*.js', ['lint', 'scripts']);
+    gulp.watch('static/sass/**/*.scss', ['compass']);
+});
+
+gulp.task('default', ['scripts', 'compass', 'images', 'watch']);
+
+
