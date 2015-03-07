53580c5b5aa8d9b50d85868116304bfe8d77c451
Robin Rendle <desk@robinrendle.com>
2015-03-07 13:32:15 +0000

    fixed the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,23 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    compass = require('gulp-compass'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    uglifycss = require('gulp-uglifycss'),
+    minifyCSS = require('gulp-minify-css'),
+    prefix = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
-    uncss = require('gulp-uncss'),
-    webp = require('gulp-webp'),
-    imageop = require('gulp-image-optimization');
-
+    sass = require('gulp-sass'),
+    size = require('gulp-size'),
+    imageop = require('gulp-image-optimization'),
+    watch = require('gulp-watch');
 
 gulp.task('images', function(cb){
     gulp.src(['static/images/**/*.png','static/images/**/*.jpg', 'static/images/**/*.gif','static/images/**/*.jpeg'])
         .pipe(imageop({
-            optimizationLevel: 6, 
+            optimizationLevel: 6,
             progressive: true,
             interlaced: true
         }))
-        .pipe(gulp.dest('build/images/')).on('end', cb).on('error', cb)
-        .pipe(webp())
+        .pipe(gulp.dest('build/images/')).on('error', cb)
         .pipe(gulp.dest('build/images'));
 });
 
@@ -33,43 +32,41 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('compass', function(){
-    return gulp.src('static/sass/*.scss')
-        .pipe(compass({
-            config_file: './config.rb',
-            css: 'build/css',
-            sass: 'static/sass'
+// Compiles scss into the build/css dir
+gulp.task('sass', function(){
+    gulp.src('./static/sass/*.scss')
+        .pipe(sass({
+            errLogToConsole: true
         }))
-        .pipe(gulp.dest('build/css'))
-        .pipe(concat('style.min.css'))
-        .pipe(gulp.dest('build/css'))
-        .pipe(uglifycss())
-        .pipe(gulp.dest('build/css'));
+        .pipe(prefix())
+        .pipe(gulp.dest('./build/css'))
 });
 
-gulp.task('work-css', function(){
-    return gulp.src('build/css/screen.css')
-        .pipe(concat('work-styles.min.css'))
-        .pipe(uncss({
-            html: [
-                'http://robinrendle.com/work',
-                'http://robinrendle.com/work/erskine-design',
-                'http://robinrendle.com/work/endsleigh',
-                'http://robinrendle.com/work/nottinghamshire-council',
-                'http://robinrendle.com/work/simon-campbell'
-            ]
-        }))
-        .pipe(gulp.dest('build/css'))
-        .pipe(uglifycss()) 
-        .pipe(gulp.dest('build/css'));
+// Autoprefixes on minified CSS
+gulp.task('prefix', function(){
+    gulp.src('./build/css/*.css')
+        .pipe(prefix())
+        .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch('static/js/*.js', ['lint', 'scripts']);
-    gulp.watch('static/sass/**/*.scss', ['compass']);
-    gulp.watch('static/sass/*.scss', ['compass']);
+// Minifies CSS
+gulp.task('minify', function(){
+    gulp.src('./build/css/screen.css')
+        .pipe(size({gzip: true, showFiles: true, title:'minified screen.css'}))
+        .pipe(minifyCSS())
+        .pipe(rename('screen.min.css'))
+        .pipe(gulp.dest('./build/css/'))
+        .pipe(size({gzip: true, showFiles: true, title:'minified screen.css'}));
 });
 
-gulp.task('default', ['scripts', 'compass', 'watch']);
+gulp.task('start', function(){
+    gulp.watch('./static/js/*.js', ['lint', 'scripts']);
+    gulp.watch('./static/sass/**/*.scss', ['sass']);
+    gulp.watch('./static/sass/*.scss', ['sass']);
+    gulp.watch('./build/css/screen.css', ['minify']);
+});
 
-gulp.task('build', ['images']);
+gulp.task('build', ['images', 'minify', 'sass']);
+
+gulp.task('default', ['build', 'start']);
+
