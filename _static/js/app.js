40cf463b1c1bbedfe93a34d71b6d4ef2f160cd57
Robin Rendle <robinrendle@Robins-MBP.home>
2016-02-13 15:23:36 +0000

    fixes to the essay

--- _static/js/app.js
+++ _static/js/app.js
@@ -1,5 +1,25 @@
 (function(){
 
+    var Dropdown = (function(){
+        var Menu = {},
+        menuElem = document.querySelector('.m-banner__menu'),
+        bannerElem = document.querySelector('.m-banner');
+
+        var bind = function(){
+            if(menuElem){
+                menuElem.addEventListener('click', function(e){
+                    e.preventDefault();
+                    bannerElem.classList.toggle('is--active');
+                });
+            }
+        }
+
+        Menu.init = function(){
+            bind();
+        }
+        return Menu;
+    })();
+
     var App = {
         init: function(){
             Dropdown.init();
