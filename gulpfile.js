d3df912caf58ab28904a0c82d1d64df8dceb5777
Robin Rendle <robin.rendle@gusto.com>
2018-12-29 22:32:05 -0800

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,105 @@
 var gulp = require("gulp"),
-	shell = require("gulp-shell"),
-	jshint = require("gulp-jshint"),
-	browserSync = require("browser-sync").create(),
-	autoprefixer = require("gulp-autoprefixer"),
-	sass = require("gulp-sass"),
-	watch = require("gulp-watch"),
-	imagemin = require("gulp-imagemin"),
-	pngquant = require("imagemin-pngquant"),
-	uglify = require("gulp-uglify"),
-	concat = require("gulp-concat"),
-	cp = require("child_process");
+  shell = require("gulp-shell"),
+  jshint = require("gulp-jshint"),
+  browserSync = require("browser-sync").create(),
+  autoprefixer = require("gulp-autoprefixer"),
+  sass = require("gulp-sass"),
+  imagemin = require("gulp-imagemin"),
+  pngquant = require("imagemin-pngquant"),
+  uglify = require("gulp-uglify"),
+  concat = require("gulp-concat"),
+  log = require('fancy-log'),
+  jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll',
+  cp = require("child_process");
 
-// rename = require('gulp-rename'),
-// size = require('gulp-size'),
 
-gulp.task("scripts", function() {
-	gulp
-		.src([
-			"_static/js/lib/*.js",
-			"_static/js/modules/dropdown.js",
-			"_static/js/app.js"
-		])
-		.pipe(uglify())
-		.pipe(gulp.dest("js"))
-		.pipe(browserSync.reload({ stream: true }));
-	gulp
-		.src(["._static/js/lib/prism.js"])
-		.pipe(uglify())
-		.pipe(gulp.dest("js"))
-		.pipe(browserSync.reload({ stream: true }));
-});
+////////////////////////////////////////////////////////////////
+// SCSS / JS
+////////////////////////////////////////////////////////////////
 
-gulp.task("images", function(cb) {
-	return gulp
-		.src([
-			"_static/images/*.*",
-			"_static/images/**/*.*",
-			"_static/images/**/**/*.*"
-		])
-		.pipe(
-			imagemin({
-				progressive: true,
-				svgoPlugins: [{ removeViewBox: false }],
-				use: [pngquant()]
-			})
-		)
-		.pipe(gulp.dest("build/images"))
-		.on("error", cb);
-});
+// gulp.task("scripts", function() {
+//   gulp
+//     .src([
+//       './_static/js/lib/*.js',
+//       './_static/js/app.js'
+//     ])
+//     .pipe(uglify())
+//     .pipe(gulp.dest('js'))
+//     .pipe(browserSync.reload({ stream: true }));
+//   gulp
+//     .src(['./_static/js/lib/prism.js'])
+//     .pipe(uglify())
+//     .pipe(gulp.dest('js'))
+//     .pipe(browserSync.reload({ stream: true }));
+// });
 
-gulp.task("jekyll-dev", function(done) {
-	return cp.spawn("jekyll", ["build"], { stdio: "inherit" }).on("close", done);
-});
 
-gulp.task("jekyll-rebuild", ["jekyll-dev"], function() {
-	browserSync.reload();
-});
+var paths = {
+  styles: {
+    src: '_static/sass/**/*.scss',
+    dest: '_site/css',
+    destsecond: 'css'
+  },
+  images: {
+    src: '_static/images/**/*.*',
+    dest: 'build/images'
+  }
+};
 
-gulp.task("browser-sync", ["sass", "scripts", "jekyll-dev"], function() {
-	browserSync.init({
-		server: "_site",
-		port: 1234
-	});
-});
+function style() {
+  return gulp.src(paths.styles.src)
+    .pipe(sass({
+       includePaths: ['scss'],
+       outputStyle: 'expanded',
+       onError: browserSync.notify
+    }))
+    .pipe(autoprefixer(['last 3 versions', '> 1%', 'ie 9'], { cascade: true }))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest(paths.styles.destsecond));
+}
 
-gulp.task("sass", function() {
-	gulp
-		.src("_static/sass/*.scss")
-		.pipe(
-			sass({
-				includePaths: ["scss"],
-				onError: browserSync.notify
-			})
-		)
-		.pipe(autoprefixer(["last 3 versions", "> 1%", "ie 9"], { cascade: true }))
-		.pipe(gulp.dest("_site/css"))
-		.pipe(browserSync.reload({ stream: true }))
-		.pipe(gulp.dest("css"));
-});
+function images(cb) {
+  return gulp.src(paths.images.src)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(paths.images.src)).on('error', cb);
+}
 
-gulp.task("watch", function() {
-	gulp.watch(["_static/sass/**/*.scss", "_static/sass/*.scss"], ["sass"]);
-	gulp.watch(
-		[
-			"*.html",
-			"_layouts/*.html",
-			"**.*.html",
-			"_includes/*.html",
-			"_projects/*.markdown",
-			"_posts/*"
-		],
-		["jekyll-rebuild"]
-	);
-	gulp.watch(["_uploads/*"], ["jekyll-rebuild"]);
-	gulp.watch(["_static/images/*", "_static/images/*/*"], ["images"]);
-});
+function browserSyncServe(done) {
+  browserSync.init({
+    server: {
+      baseDir: "_site"
+    }
+  })
+  done();
+}
 
-// gulp.task('build', [''])
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
+}
 
-gulp.task("default", ["browser-sync", "images", "watch"]);
+function jekyllBuild() {
+  return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+}
+
+function watch() {
+  gulp.watch(paths.styles.src, style)
+  gulp.watch(paths.images.src, images)
+  gulp.watch(
+    [
+    '*.html',
+    '_layouts/*.html',
+    '/**.*.html',
+    '_includes/*.html',
+    '_projects/*.markdown',
+    '_posts/*'
+  ],
+  gulp.series(jekyllBuild, browserSyncReload));
+}
+
+gulp.task('default', gulp.parallel(jekyllBuild, browserSyncServe, watch));
