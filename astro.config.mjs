52c56ab2f7026a8583e90a5c3f1b7f20d61769bb
robinrendle <robinjrendle@gmail.com>
2021-08-02 17:43:12 -0700

    fixed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages',   // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  projectRoot: ".", // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  pages: "./src/pages", // Path to Astro components, pages, and data
+  dist: "./dist", // When running `astro build`, path to final static output
+  public: "./public", // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
     site: "http://robinrendle.com", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     // sitemap: true,      // Generate sitemap (set to "false" to disable)
