f03e3411db170eef25bbf1d07bd153c29eb5642a
Robin Rendle <robinjrendle@gmail.com>
2023-08-13 18:03:52 -0700

    fixed dates that have annoyed me for years

--- .eleventy.js
+++ .eleventy.js
@@ -30,6 +30,16 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  eleventyConfig.addFilter("projectsDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("MMMM yyyy");
+  });
+
+  eleventyConfig.addFilter("fullDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "MMMM d, yyyy"
+    );
+  });
+
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
